3a31f8ecb868dc258bbf4779ec9adde83b9ff94c
Kondrat Shmoylov <kondrat.shmoylov@gmail.com>
2017-05-09 09:34:50 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,88 @@
+# ember-cli-less-pods
+
+Styling your pods with the less style file in the pod directory.
+Based on [ember-cli-sass-pods](https://github.com/justtal/ember-cli-sass-pods) implementation
+ 
+## Example of usage ember-cli-less-pods
+Say you have a contacts route and contact-box component.
+
+##### Generate regular route and component:
+```
+ember g route contacts -p
+ember g component contact-box -p
+```
+##### Then, use ember-cli-less-pods power and generate styles:
+```
+ember g less contacts -p
+ember g less components/contact-box -p
+```
+
+##### Your file structure would be:
+```
+app/
+
+app/contacts
+app/contacts/route.js
+app/contacts/template.hbs
+app/contacts/style.less
+
+app/components/
+app/components/contact-box
+app/components/contact-box/component.js
+app/components/contact-box/template.hbs
+app/components/contact-box/style.less
+
+app/styles/
+app/styles/app.less
+app/styles/[importFileName].less
+```
+##### app/styles/[importFileName].less
+```
+@import "contacts/style";
+@import "components/contact-box/style";
+```
+
+## Installation
+
+##### Install ember-cli-less-pods
+
+* `ember install ember-cli-less-pods`
+
+It will automatically install [ember-cli-less](https://github.com/aexmachina/ember-cli-less#ember-cli-less) less preprocess package.
+
+##### The import file
+You need to add the import file into your main app less file.
+
+If you use podModulePrefix your imports file would be:
+```
+[podModulePrefix].less
+```
+otherwise it would be:
+```
+pods.less
+```
+Add import line into your main app less file:
+
+```
+@import "[podModulePrefix] or pods";
+```
+
+##### Watched folder
+Add paths for watching in the 'ember.cli-build.js' (for Ember 2+ ember-cli-build.js):
+```
+var app = new EmberApp(defaults, {
+  lessOptions: {
+    includePaths: ['app']
+  }
+});
+```
+
+## Usage
+
+To generate styles into your pod - just run:
+
+```
+ember g style [path] -p
+```
+
+Enjoy styling
---
+++ blueprints/style/index.js
@@ -0,0 +1,92 @@
+var fs   = require('fs-extra');
+var path = require('path');
+var mkdirp = require('mkdirp');
+var util = require('util');
+
+module.exports = {
+  podsDir: '',
+
+  description: 'Generates a style.less file',
+
+  beforeInstall: function (options, locals) {
+      // replace \ with / for compatibility with windows-style nested paths
+      this.podsDir = locals.fileMap.__path__.replace(/\\/g, '/');
+      this.podsDir = this.podsDir.replace('/' + options.entity.name, '');
+      this.podsDir = this.podsDir.replace(options.entity.name, '');
+
+      if (!options.taskOptions.pod) {
+          throw new Error('You must use pods with ember-cli-less-pods. Run with --pod.');
+      }
+  },
+
+  // locals: function(options) {
+  //   // Return custom template variables here.
+  //   return {
+  //     foo: options.entity.options.foo
+  //   };
+  // }
+
+  afterInstall: function (options) {
+      //console.log(util.inspect(options.project, false, null));
+      var entity = options.entity;
+
+      addScssToImportFile(entity.name, {
+          name: entity.name,
+          root: options.project.root,
+          podsDir: this.podsDir
+      });
+  },
+
+  afterUninstall: function(options) {
+        var entity = options.entity;
+
+        removeScssFromImportFile(entity.name, {
+          name: entity.name,
+          root: options.project.root,
+          podsDir: this.podsDir
+        });
+    }
+};
+
+function setSharedOpts(options) {
+    var opts = {};
+    opts.importFile = options.podsDir ? options.podsDir.replace(/(\\|\/)$/, '') : 'pods';
+    opts.filePath = path.join(options.root, 'app/styles');
+    opts.importScssPath = path.join(opts.filePath, opts.importFile + '.less');
+    opts.podsDir = options.podsDir ? opts.importFile + '/' : '';
+    opts.newLine = '@import "app/' + opts.podsDir + options.name + '/style";\n';
+    return opts;
+}
+
+function addScssToImportFile (name, options) {
+    var sharedOpts = setSharedOpts(options),
+        source;
+
+    if (!fs.existsSync(sharedOpts.filePath)) {
+        mkdirp(sharedOpts.filePath);
+    }
+
+    if (!fs.existsSync(sharedOpts.importScssPath)) {
+        fs.writeFileSync(sharedOpts.importScssPath, sharedOpts.newLine, 'utf8');
+    } else {
+        source = fs.readFileSync(sharedOpts.importScssPath, 'utf-8');
+        source += sharedOpts.newLine;
+        fs.writeFileSync(sharedOpts.importScssPath, source);
+    }
+}
+
+function removeScssFromImportFile(name, options) {
+    var sharedOpts = setSharedOpts(options),
+        source;
+
+    source = fs.readFileSync(sharedOpts.importScssPath, 'utf-8');
+    sourceArray = source.split('\n');
+    sourceArrayMatchedIndex = sourceArray.indexOf(sharedOpts.newLine.replace('\n', ''));
+
+    if (sourceArrayMatchedIndex > -1) {
+        delete sourceArray[sourceArrayMatchedIndex];
+    }
+    
+    source = sourceArray.filter(Boolean).join('\n').trim() + '\n';
+    fs.writeFileSync(sharedOpts.importScssPath, source);
+}
